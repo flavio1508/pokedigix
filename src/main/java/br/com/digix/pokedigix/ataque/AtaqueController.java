2f3bab995b7b979fb5a1998c50effa50a8e527d6
Flávio Augusto <flavioaugusto1508@gmail.com>
2022-08-17 17:51:01 -0400

    atualização

--- src/main/java/br/com/digix/pokedigix/ataque/AtaqueController.java
+++ src/main/java/br/com/digix/pokedigix/ataque/AtaqueController.java
@@ -9,18 +9,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.digix.pokedigix.tipo.Tipo;
+import br.com.digix.pokedigix.tipo.TipoRepository;
+import br.com.digix.pokedigix.tipo.TipoRequestDTO;
 import br.com.digix.pokedigix.tipo.TipoResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
-@RestController
 @RequestMapping(path = {"/api/v1/ataques"}, produces = {"application/json"})
-
 public class AtaqueController {
     @Autowired
-    private AtaqueRepository ataqueRepository;
-    
+        private AtaqueRepository ataqueRepository;
+        @Autowired 
+        private TipoRepository tipoRepository;
     @Operation(summary = "Buscar por ID")
     @ApiResponse(responseCode = "200", description = "Retorna os dados do ataque solicitado")
     @GetMapping(path = "/{id}")
@@ -35,12 +37,12 @@ public class AtaqueController {
     @ApiResponse(responseCode = "201", description = "Ataque criado usando o Tipo")
     @PostMapping(consumes = {"application/json"})
     public ResponseEntity<AtaqueResponseDTO> criarAtaque(@RequestBody AtaqueRequestDTO novoAtaque) throws Exception{
-        Tipo tipo = ataqueRepository.findById(novoAtaque.getTipoId()).get();
-        Ataque ataque = new Ataque(novoAtaque.getNome(), novoAtaque.getForca(), novoAtaque.getAcuracia(), novoAtaque.getPontosDePoder(), novoAtaque.getDescricao(), novoAtaque.getCategoria(), tipo);
+        Tipo tipo = tipoRepository.findById(novoAtaque.getTipoId()).get();
+        Ataque ataque = new Ataque(novoAtaque.getNome(),novoAtaque.getDescricao(),novoAtaque.getCategoria(),novoAtaque.getForca(),novoAtaque.getAcuracia(),novoAtaque.getPontosDePoder(),tipo);
         ataqueRepository.save(ataque);
 
         TipoResponseDTO tipoResponseDTO = new TipoResponseDTO(tipo.getId(), tipo.getNome());
-        return ResponseEntity.status(HttpStatus.CREATED).body( new AtaqueResponseDTO(ataque.getId(),ataque.getNome(), ataque.getDescricao(),ataque.getCategoria(), ataque.getForca(),ataque.getAcuracia(), ataque.getPontosDePoder(), tipo));
+        return ResponseEntity.status(HttpStatus.CREATED).body( new AtaqueResponseDTO(ataque.getId(),ataque.getNome(), ataque.getDescricao(),ataque.getCategoria(), ataque.getForca(),ataque.getAcuracia(), ataque.getPontosDePoder(), tipoResponseDTO));
     }
     
    
